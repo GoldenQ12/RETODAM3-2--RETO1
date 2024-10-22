7b9d6e2513cfc71177fcb422427757f55f57c0a9
GoldenQ12 <goldenytstorm@gmail.com>
2024-10-22 21:18:55 +0200

    #17 - Primer deploy de Aplicacion

---
+++ RETO1/Componente/Componente/Componente.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Componente
+    ' This code goes in your Form1 class
+
+    ' Function to resize the image
+    Private Function ResizeImage(ByVal img As Image, ByVal width As Integer, ByVal height As Integer) As Image
+        Dim bmp As New Bitmap(width, height)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.DrawImage(img, 0, 0, width, height)
+        End Using
+        Return bmp
+    End Function
+
+    ' Function to download the image from URL
+    Private Function DownloadImageFromUrl(ByVal url As String) As Image
+        Dim image As Image = Nothing
+        Try
+            Dim webClient As New WebClient()
+            Using stream As Stream = webClient.OpenRead(url)
+                image = Image.FromStream(stream)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error downloading image: " & ex.Message)
+        End Try
+        Return image
+    End Function
+
+    ' In your Form Load event or Button click event:
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' URL of the image
+        Dim url As String = "https://cdn-icons-png.flaticon.com/512/5621/5621813.png"
+
+        ' Download the image from the URL
+        Dim originalImage As Image = DownloadImageFromUrl(url)
+
+        If originalImage IsNot Nothing Then
+            ' Resize the image (for example, 50x50)
+            Dim resizedImage As Image = ResizeImage(originalImage, 96, 96)
+
+            ' Display the resized image in the PictureBox
+            PictureBox1.Image = resizedImage
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
---
+++ RETO1/RETO1/RETO1/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SplitContainer1 = New SplitContainer()
+        Panel3 = New Panel()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel1.SuspendLayout()
+        SplitContainer1.Panel2.SuspendLayout()
+        SplitContainer1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 0)
+        SplitContainer1.Name = "SplitContainer1"
+        ' 
+        ' SplitContainer1.Panel1
+        ' 
+        SplitContainer1.Panel1.BackColor = SystemColors.AppWorkspace
+        SplitContainer1.Panel1.Controls.Add(Panel3)
+        ' 
+        ' SplitContainer1.Panel2
+        ' 
+        SplitContainer1.Panel2.BackColor = SystemColors.ActiveBorder
+        SplitContainer1.Panel2.Controls.Add(FlowLayoutPanel1)
+        SplitContainer1.Size = New Size(1584, 861)
+        SplitContainer1.SplitterDistance = 528
+        SplitContainer1.TabIndex = 0
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BorderStyle = BorderStyle.FixedSingle
+        Panel3.Location = New Point(-12, 652)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(546, 209)
+        Panel3.TabIndex = 0
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Location = New Point(3, 6)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(923, 634)
+        FlowLayoutPanel1.TabIndex = 0
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1584, 861)
+        Controls.Add(SplitContainer1)
+        Name = "Form1"
+        Text = "Form1"
+        SplitContainer1.Panel1.ResumeLayout(False)
+        SplitContainer1.Panel2.ResumeLayout(False)
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Panel3 As Panel
+
+End Class
