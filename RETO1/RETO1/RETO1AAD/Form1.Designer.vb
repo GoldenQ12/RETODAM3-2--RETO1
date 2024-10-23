ecf65269d9a7da26778bff1b4134c9f4f46db099
GoldenQ12 <goldenytstorm@gmail.com>
2024-10-23 11:35:44 +0200

    #17 - ?

--- RETO1/RETO1/RETO1AAD/Form1.Designer.vb
+++ RETO1/RETO1/RETO1AAD/Form1.Designer.vb
@@ -25,18 +25,17 @@ Partial Class Form1
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
-        Me.UserControl12 = New ComponenteItem.UserControl1()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
-        Me.FlowLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'SplitContainer1
         '
         Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Margin = New System.Windows.Forms.Padding(2)
         Me.SplitContainer1.Name = "SplitContainer1"
         '
         'SplitContainer1.Panel1
@@ -48,48 +47,40 @@ Partial Class Form1
         '
         Me.SplitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ActiveBorder
         Me.SplitContainer1.Panel2.Controls.Add(Me.FlowLayoutPanel1)
-        Me.SplitContainer1.Size = New System.Drawing.Size(1810, 918)
-        Me.SplitContainer1.SplitterDistance = 749
-        Me.SplitContainer1.SplitterWidth = 5
+        Me.SplitContainer1.Size = New System.Drawing.Size(1358, 746)
+        Me.SplitContainer1.SplitterDistance = 561
         Me.SplitContainer1.TabIndex = 0
         '
         'Panel3
         '
         Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel3.Location = New System.Drawing.Point(-14, 695)
+        Me.Panel3.Location = New System.Drawing.Point(-10, 565)
+        Me.Panel3.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(624, 223)
+        Me.Panel3.Size = New System.Drawing.Size(468, 182)
         Me.Panel3.TabIndex = 0
         '
         'FlowLayoutPanel1
         '
-        Me.FlowLayoutPanel1.Controls.Add(Me.UserControl12)
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(3, 6)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(2, 5)
+        Me.FlowLayoutPanel1.Margin = New System.Windows.Forms.Padding(2)
         Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(1341, 690)
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(1006, 561)
         Me.FlowLayoutPanel1.TabIndex = 0
-        '
-        'UserControl12
-        '
-        Me.UserControl12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.UserControl12.Location = New System.Drawing.Point(3, 3)
-        Me.UserControl12.Name = "UserControl12"
-        Me.UserControl12.Size = New System.Drawing.Size(285, 512)
-        Me.UserControl12.TabIndex = 1
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1810, 918)
+        Me.ClientSize = New System.Drawing.Size(1358, 746)
         Me.Controls.Add(Me.SplitContainer1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.SplitContainer1.Panel1.ResumeLayout(False)
         Me.SplitContainer1.Panel2.ResumeLayout(False)
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
-        Me.FlowLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -97,5 +88,4 @@ Partial Class Form1
     Friend WithEvents SplitContainer1 As SplitContainer
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
     Friend WithEvents Panel3 As Panel
-    Friend WithEvents UserControl12 As ComponenteItem.UserControl1
 End Class
